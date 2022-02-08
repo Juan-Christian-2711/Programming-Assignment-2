be455ea61b1772fc051715688aac29cb9851d071
Juan <Juanchristian2002@outlook.com>
2022-02-08 12:09:24 +0000

    Making book accessible to other classes

--- src/com/company/Books.java
+++ src/com/company/Books.java
@@ -2,12 +2,47 @@ package com.company;
 
 public class Books {
     private int BookID;
-
+    private String Title;
+    private int pages;
+    private String Genre;
+    private boolean NonFiction;
     public int getBookID() {
         return BookID;
     }
 
+    public String getTitle() {
+        return Title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public boolean isNonFiction() {
+        return NonFiction;
+    }
+
     public void setBookID(int bookID) {
         this.BookID = bookID;
+    }
+
+    public void setTitle(String title) {
+        this.Title = title;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public void setNonFiction(boolean non) {
+        NonFiction = non;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args){
         Books Book = new Books();
         Book.setBookID(1);
-        System.out.println(Book.getBookID());
+        Book.setTitle("Watchmen");
+        Book.setGenre("Graphic Novel");
+        Book.setNonFiction(false);
+        Book.setPages(414);
+        System.out.println("Book ID: " +Book.getBookID() + "\nBook Genre: " + Book.getGenre() + "\nPages: " + Book.getPages() + "\nTitle: " + Book.getTitle() + "\nNon-Fiction?: " + Book.isNonFiction());
     }
 }
