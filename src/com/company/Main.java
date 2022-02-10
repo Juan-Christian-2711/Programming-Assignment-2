4f877b374e2c420c38ca5db14f7e3f4780a0b880
Juan <Juanchristian2002@outlook.com>
2022-02-10 16:56:33 +0000

    Adding in Program CLI structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,42 @@
 package com.company;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
+        InputBookDetails();
+    }
+    public static void InputBookDetails(){
         Books Book = new Books();
-        Book.setBookID("1");
-        Book.setTitle("Watchmen");
-        Book.setGenre("Graphic Novel");
-        Book.setPages(414);
-        Book.writeToCSV();
-        System.out.println("Book ID: " +Book.getBookID() + "\nBook Genre: " + Book.getGenre() + "\nPages: " + Book.getPages() + "\nTitle: " + Book.getTitle());
+        Scanner Scanner = new Scanner(System.in);
+        String[] enterPhrase = {"Enter Book ID: ", "Enter Title: ", "Enter Genre: ","Enter Pages: "};
+        List<String> BookDetails=new ArrayList<String>();
+        for (String s : enterPhrase) {
+            System.out.println(s);
+            String Item = Scanner.nextLine();
+            BookDetails.add(Item);
+        }
+        Book.setBookID(BookDetails.get(0));
+        Book.setTitle(BookDetails.get(1));
+        Book.setGenre(BookDetails.get(2));
+        Book.setPages(Integer.parseInt(BookDetails.get(3)));
+        Book.writeToJSON(Book.getBookID(), Book.getTitle(), Book.getPages(), Book.getGenre());
+        boolean notcomplete = true;
+        while (notcomplete){
+            System.out.println("Would you like to add another book? (y/n)");
+            String answer = Scanner.nextLine();
+            if(answer.equals("y")){
+                notcomplete = false;
+                InputBookDetails();
+            }
+            else if (answer.equals("n")){
+                notcomplete = false;
+                return;
+            }
+            else{
+                System.out.println("Incorrect input:");
+                System.out.println(answer);
+            }
+        }
+
     }
 }
