e6196cccac8105a9f088ffa707b4ed7f7e841bb7
Juan <Juanchristian@outlook.com>
2022-03-03 14:24:20 +0000

    Restructuring classes to make the source code more organised.

--- src/com/company/Books.java
+++ src/com/company/Books.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Books implements Serializable {
     private String BookID;
@@ -53,5 +54,58 @@ public class Books implements Serializable {
             System.out.println("Unable to create object output stream: " + e.getMessage());
         }
 
+    }
+    public static List<String> loadBooks(){
+        try {
+            FileInputStream FIS = new FileInputStream("save.dat");
+            ObjectInputStream OIS = new ObjectInputStream(FIS);
+            List<String> Books = (List<String>)OIS.readObject();
+            return Books;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error converting data to object" + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.print("Cant find class representing object");
+        }
+
+        return new ArrayList<String>();
+    }
+    public static void readBooks(){
+        List<String> Books = loadBooks();
+        System.out.println(Books);
+    }
+
+    public static void InputBookDetails(){
+        Books Book = new Books();
+        Scanner Scanner = new Scanner(System.in);
+        String[] enterPhrase = {"Enter Book ID: ", "Enter Title: ", "Enter Genre: ","Enter Pages: "};
+        List<String> BookDetails=new ArrayList<>();
+        for (String s : enterPhrase) {
+            System.out.println(s);
+            String Item = Scanner.nextLine();
+            BookDetails.add(Item);
+        }
+        Book.setBookID(BookDetails.get(0));
+        Book.setTitle(BookDetails.get(1));
+        Book.setGenre(BookDetails.get(2));
+        Book.setPages(Integer.parseInt(BookDetails.get(3)));
+        saveBook(BookDetails);
+        boolean notcomplete = true;
+        while (notcomplete){
+            System.out.println("Would you like to add another book? (y/n)");
+            String answer = Scanner.nextLine();
+            if(answer.equals("y")){
+                notcomplete = false;
+                InputBookDetails();
+            }
+            else if (answer.equals("n")){
+                return;
+            }
+            else{
+                System.out.println("Incorrect input:");
+                System.out.println(answer);
+            }
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,11 @@
 package com.company;
-import java.awt.print.Book;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.*;
-
-import static com.company.Books.saveBook;
+import static com.company.Books.*;
 
 public class Main {
     public static void main(String[] args){
-
         //runs main menu
         MainMenu();
-        readBooks();
     }
     public static void MainMenu(){
         //Prompts user to choose what to do
@@ -36,57 +28,6 @@ public class Main {
         }
 
     }
-    public static void InputBookDetails(){
-        Books Book = new Books();
-        Scanner Scanner = new Scanner(System.in);
-        String[] enterPhrase = {"Enter Book ID: ", "Enter Title: ", "Enter Genre: ","Enter Pages: "};
-        List<String> BookDetails=new ArrayList<>();
-        for (String s : enterPhrase) {
-            System.out.println(s);
-            String Item = Scanner.nextLine();
-            BookDetails.add(Item);
-        }
-        Book.setBookID(BookDetails.get(0));
-        Book.setTitle(BookDetails.get(1));
-        Book.setGenre(BookDetails.get(2));
-        Book.setPages(Integer.parseInt(BookDetails.get(3)));
-        saveBook(BookDetails);
-        boolean notcomplete = true;
-        while (notcomplete){
-            System.out.println("Would you like to add another book? (y/n)");
-            String answer = Scanner.nextLine();
-            if(answer.equals("y")){
-                notcomplete = false;
-                InputBookDetails();
-            }
-            else if (answer.equals("n")){
-                return;
-            }
-            else{
-                System.out.println("Incorrect input:");
-                System.out.println(answer);
-            }
-        }
-    }
-    public static List<String> loadBooks(){
-        try {
-            FileInputStream FIS = new FileInputStream("save.dat");
-            ObjectInputStream OIS = new ObjectInputStream(FIS);
-            List<String> Books = (List<String>)OIS.readObject();
-            return Books;
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Error converting data to object" + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.out.print("Cant find class representing object");
-        }
 
-        return new ArrayList<String>();
-    }
-    public static void readBooks(){
-        List<String> Books = loadBooks();
-        System.out.println(Books);
-    }
 }
 
